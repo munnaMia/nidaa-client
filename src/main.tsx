4060710421c600dc3baf6476f4e39b7568f043b5
munnaMia <mdmunnamia987@gmail.com>
2026-08-12 14:38:15 +0600

    feat: add react-router-dom for routing and update main.tsx for routing setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { RouteProvider } from '@/providers/route-provider';
+import { ThemeProvider } from '@/providers/theme-provider';
+import App from '@/App';
+import '@/styles/globals.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <RouteProvider>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </RouteProvider>
+        </BrowserRouter>
+    </React.StrictMode>
+);
